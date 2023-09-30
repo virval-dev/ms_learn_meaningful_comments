250d7f2b5773bb549853158f3f4b72723b81722b
vivaldev <valtteri.virtanen.dev@gmail.com>
2023-09-30 11:52:40 +0300

    Variable naming conventions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
